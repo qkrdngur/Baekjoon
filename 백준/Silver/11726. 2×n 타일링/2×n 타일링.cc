01bcc8b117de599746c59cd63a2b433bb5a7d490
qkrdngur <98994220+qkrdngur@users.noreply.github.com>
2023-12-14 00:03:31 +0900

    [Silver III] Title: 2×n 타일링, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int DP[1002] = { 0, 1, 2 };
+
+int Tile(int n)
+{
+    if (n <= 0)
+        return 0;
+    if (DP[n] != 0)
+        return DP[n];
+
+    DP[n] = (Tile(n - 1) + Tile(n - 2)) % 10007;
+    return DP[n];
+}
+
+int main()
+{
+    int num, answer;
+    cin >> num;
+
+    cout << Tile(num);
+}
